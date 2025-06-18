fc34f7ec42102a93a0a5240c920a1eb021382b5d
LakshanaPM <pmlakshana123@gmail.com>
2025-06-18 18:15:15 +0530

    Add files via upload

---
+++ PGLife/place.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/place.css"/>
+        <title>Hostels</title>
+    </head>
+    <body>
+        <img class="logo" src="img/logo.png" alt="PG Life" width="100" height="50">
+        <input type="button" class="signup">Signup</input>
+        <input type="button" class="signup">Login</input>
+        <div class="container">
+            <div class="circle">
+                <a href="each.html" target="_blank">
+                  <img src="img/filter.png" alt="Clickable Circle">Filter
+                </a>
+            </div>
+            <div class="circle">
+                <a href="https://www.example.com" target="_blank">
+                  <img src="img/asc.png" alt="Clickable Circle">Highest rent first
+                </a>
+            </div>
+            <div class="circle">
+                <a href="https://www.example.com" target="_blank">
+                  <img src="img/desc.png" alt="Clickable Circle">Lowest rent first
+                </a>
+            </div>
+        </div>
+    </body>
+</html>
